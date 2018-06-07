ec3467a0106a70d1ab799dd77cbc9b0cc7cc5bdc
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-06-07 08:43:53 -0500

    base styles/content for footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,27 +1,11 @@
-import Link from "next/link";
-
-const hrStyle = {
-    marginTop: 75
-};
+import Link from 'next/link';
 
 const Footer = () => (
-    <footer>
-        <hr style={hrStyle} />
-        <p>
-            ❤️{" "}
-            <Link href="https://postlight.com">
-                <a>Made by Postlight</a>
-            </Link>. 🍴{" "}
-            <Link href="https://github.com/postlight/headless-wp-starter">
-                <a>Fork on GitHub</a>
-            </Link>.
-        </p>
-        <p>
-            <Link href="mailto:hello@postlight.com?subject=Partner+with+Postlight+on+a+headless+CMS+project">
-                <a>Say hi.</a>
-            </Link>
-        </p>
-    </footer>
+  <footer>
+    <p className="copyright">
+      Copyright © 2018 <span>Jacob Mishkin</span>
+    </p>
+  </footer>
 );
 
 export default Footer;
