21bc2e89c6170b7b221ea271d6a2e0a580f6f2f0
Jacob Mishkin <user@MacBook-Pro-3.local>
2018-06-26 13:30:41 -0500

    Removed excerpt_more ascii html

--- wordpress/wp-content/themes/postlight-headless-wp/inc/helper.php
+++ wordpress/wp-content/themes/postlight-headless-wp/inc/helper.php
@@ -12,3 +12,9 @@ remove_filter( 'the_excerpt', 'wpautop' );
 
 // Add Theme Support for Featured Image
 add_theme_support('post-thumbnails');
+
+// This will remove the HTML tags [&hellp] from excerpt
+	function headless_custom_excerpt( $more ) {
+		return '';
+	}
+	add_filter('excerpt_more', 'headless_custom_excerpt');
